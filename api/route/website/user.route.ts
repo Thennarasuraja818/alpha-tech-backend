cc8afa45d47fa043b9f54d44815af3219da4efcd
Thennarasu <thennarasu@oceansoftteam.in>
2026-02-12 11:13:07 +0530

    command website user routs

--- api/route/website/user.route.ts
+++ api/route/website/user.route.ts
@@ -5,16 +5,16 @@ import { Router } from "express";
 
 import { IWebsiteUserRepository } from "../../../domain/website/user.domain";
 import { UserHandlerFun } from "../../../app/handler/website.handler/user.handler";
-import { webSiteUserServiceFun } from "../../../app/service/website/user.service";
+// import { webSiteUserServiceFun } from "../../../app/service/website/user.service";
 export function RegisterWebSiteUserRoute(
   router: Router,
   adminRepo: IWebsiteUserRepository,
   middleware: any
 ) {
-  const service = webSiteUserServiceFun(adminRepo); // Pass repository to service
-  const handler = UserHandlerFun(service); // Pass service to handler
-  router.post("/user", handler.createUser); // Define route
-  router.post("/login", handler.loginUser); // Define route
+  // const service = webSiteUserServiceFun(adminRepo); // Pass repository to service
+  // const handler = UserHandlerFun(service); // Pass service to handler
+  // router.post("/user", handler.createUser); // Define route
+  // router.post("/login", handler.loginUser); // Define route
   // // Password management routes
   // router.post("/admin-forgot-password", handler.forgotPassword);
   // router.post("/admin-reset-password", handler.resetPassword);
